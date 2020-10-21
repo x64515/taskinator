64de64f45d1269300ce9013f8aff287cb4719528
Xavier <X64515@yahoo.com>
2020-10-20 20:15:36 -0500

    taskbardataob

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
 
   event.preventDefault();
-
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  var taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
  
 };
-
-formEl.addEventListener("submit", createTaskHandler);
+var createTaskEl = function(taskDataObj) {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+  listItemEl.textContent = taskNameInput;
+  tasksToDoEl.appendChild(listItemEl);
+}
+formEl.addEventListener("submit", taskFormHandler);
